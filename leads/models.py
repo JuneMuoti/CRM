c236731b1ce558712250e614a5e980fd9859de2c
sethmuriuki <sethkrm@gmail.com>
2021-03-10 15:35:32 +0300

    add  homepage view and template files

--- leads/models.py
+++ leads/models.py
@@ -19,11 +19,15 @@ class Lead(models.Model):
     profile_picture=models.ImageField(null=True, blank=True)
     special_file=models.FileField(null=True,blank=True)
     agent=models.ForeignKey("Agent", on_delete=models.SET_NULL,null=True)
-
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
     
 class Agent(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
- 
+    def __str__(self):
+        return self.user.username
+    
    
 
     
