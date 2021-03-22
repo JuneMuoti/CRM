d195e299755bdc2e8789c0cd577616f394fe001c
sethmuriuki <june.muoti@gmail.com>
2021-03-22 18:02:29 +0300

    add static files folder

--- crm/urls.py
+++ crm/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from leads.views import home_page,HomePageView
@@ -22,4 +24,8 @@ urlpatterns = [
     path('',HomePageView.as_view(),name='home-page'),
     path('leads/' ,include('leads.urls',namespace='leads')),
     
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
