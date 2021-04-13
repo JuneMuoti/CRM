19837d96d29d18329ebf4bc2153d91b2fc7de8f8
sethmuriuki <june.muoti@gmail.com>
2021-04-13 15:23:06 +0300

    Added test files for each module and installed coverege.py

---
+++ leads/test_models.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+# Each Test Method Tests One Thing
+    # a unit test is a method that instantiates a small portion of our application and verifies its behavior independently from other parts.
+    # Integration testing is when individual software modules are combined and tested as a group
+    # , a single test should assert the behavior of a single view, model, form, method or function.
+    # 
+
+# Create your tests here.
