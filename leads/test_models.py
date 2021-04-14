37881623c48c99f923c0d94599d3c1d2597279c0
sethmuriuki <june.muoti@gmail.com>
2021-04-14 13:12:30 +0300

    tests for homepageview

--- leads/test_models.py
+++ leads/test_models.py
@@ -3,6 +3,10 @@ from django.test import TestCase
     # a unit test is a method that instantiates a small portion of our application and verifies its behavior independently from other parts.
     # Integration testing is when individual software modules are combined and tested as a group
     # , a single test should assert the behavior of a single view, model, form, method or function.
-    # 
+    # pip install coverage
+    # coverage run --source='.' manage.py test leads
+    # coverage report
+    # coverage html --omit="admin.py"
+    
 
 # Create your tests here.
--- leads/test_views.py
+++ leads/test_views.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
 # Create your tests here.
+# convention is to use prefix "test" for each teast name
+class HomePageTest(TestCase):
+
+    def test_get(self):
+        response=self.client.get(reverse("home-page"))
+        print(response)
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"home_page.html")
+        
+
+    
+ 
